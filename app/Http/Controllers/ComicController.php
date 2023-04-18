633b1da8aca93274ff0454e8458577b36849a094
paolocastaldi <pao.gamer97@gmail.com>
2023-04-18 19:40:36 +0200

    Milstone 1 complete (migration/seed/controller)

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comic;
+
+use Illuminate\Http\Request;
+
+class ComicController extends Controller
+{
+    public function index() {
+
+        $comics = Comic::all();
+
+        dd($comics);
+
+        return view('comics.index', compact('comics'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ComicController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
